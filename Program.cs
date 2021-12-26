959b4c28503f045735c0c48f64cad8547769073b
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2021-12-26 17:02:17 +0000

    create no-msbuild

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace IGAE_GUI
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			AllocConsole();
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			//ThreadManager.InitialiseThreads();
+			Application.Run(new Form_igArchiveExtractor(Config.Read()));
+			FreeConsole();
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool AllocConsole();
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool FreeConsole();
+	}
+}
